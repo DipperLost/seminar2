3180489519f1f0cf35159b0f5cfb1ec9990550bc
dipperlost <dipper>
2022-08-15 17:24:43 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
+{
+    Console.WriteLine("нет");
+}
+else if(day == 2)
+{
+    Console.WriteLine("нет");
+}
+else if(day == 3)
+{
+    Console.WriteLine("нет");
+}
+else if(day == 4)
+{
+    Console.WriteLine("нет");
+}
+else if(day == 5)
+{
+    Console.WriteLine("нет");
+}
+else if(day == 6)
+{
+    Console.WriteLine("да");
+}
+else if(day == 7)
+{
+    Console.WriteLine("да");
+}
+else if(day > 7)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
