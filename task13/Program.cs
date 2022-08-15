838bf9282b379ac29021113c3d342379f5098a51
dipperlost <dipper>
2022-08-15 18:11:49 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int C = N / 100;
+if (C >= 1)
+{
+    while (N / 100 <= 9 & N / 100 >= 1)
+    {
+        Console.WriteLine(N % 10);
+        N = N / 10;
+    }
+}
+else Console.WriteLine("третьей цифры нет");
