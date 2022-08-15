d0fa22e7c282ea05914ca65ea2532d5871a88ec1
dipperlost <dipper>
2022-08-15 17:32:52 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+int numberA = numberС % 100;
+int numberB = numberA / 10;
+Console.WriteLine(numberB);
